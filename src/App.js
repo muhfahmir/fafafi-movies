2c48aa34394429c025cbe5d6e9b0082e6ce5dbaa
Muhamad Fahmi Rahmatullah <fahmi.rahmat07@gmail.com>
2022-01-17 14:46:47 +0700

    create component header and fix breakpoint and variables scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.scss";
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+import Routes from "./config/Routes";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Router>
+      <Route
+        render={(props) => (
+          <>
+            <Header {...props} />
+            <Routes />
+            <Footer />
+          </>
+        )}
+      />
+    </Router>
+  );
 }
 
 export default App;
